3a8eb1c31b19949b79e5ca50880613238b377cec
Sohrab Radmehr <sohrab.ram95@hotmail.com>
2022-03-05 21:29:52 -0800

    basic routes setup

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require("cookie-session");
+var Keygrip = require("keygrip");
+
+const logIn = require('./route/login');
+
+
+
 
 const app = express();
-app.use(express.static('public'));
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.set("view engine", "ejs")
 
 
 
 
 
 
+app.use("/logIn", logIn);
 
 
 
 
-module.exports = {app};
+module.exports = app;
---
+++ route/login.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cookieSession = require("cookie-session");
+const router = express.Router();
+const app = require("../app");
+router.use(express.static("public"));
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
