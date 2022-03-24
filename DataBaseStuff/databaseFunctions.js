aa96c3981f0a661ccf2d2d6675af153ddd7034f0
Endmostcamera32 <90855513+Endmostcamera32@users.noreply.github.com>
2022-03-24 16:07:01 -0700

    added a function

--- DataBaseStuff/databaseFunctions.js
+++ DataBaseStuff/databaseFunctions.js
@@ -1,5 +1,17 @@
 const database = include('/databaseConnection');
 //Sohrab Stuff
+function getAllUsers(callback) {
+	let sqlQuery = "SELECT web_user_id, first_name, last_name, email FROM web_user";
+	database.query(sqlQuery, (err, results, fields) => {
+		if (err) {
+			callback(err, null);
+		}
+		else {
+			console.log(results);
+			callback(null, results);
+		}		
+	});
+}
 
 
 
@@ -20,4 +32,4 @@ const database = include('/databaseConnection');
 
 
 
-module.exports = {}
+module.exports = {getAllUsers}
--- route/homepage.js
+++ route/homepage.js
@@ -3,6 +3,9 @@ const cookieSession = require("cookie-session");
 const router = express.Router();
 const app = require("../app");
 router.use(express.static("public"));
+const database = include('./DataBaseStuff/databaseConnection');
+const dbModel = include('./DataBaseStuff/databaseFunctions');
+
 
 router.get("/", function(req, res) {
     res.render("homepage");
