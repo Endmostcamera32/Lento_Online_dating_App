2dc9718ac4b23d1f91c882679ec4e884a3c494de
Endmostcamera32 <90855513+Endmostcamera32@users.noreply.github.com>
2022-03-24 15:40:48 -0700

    1

--- DataBaseStuff/databaseFunctions.js
+++ DataBaseStuff/databaseFunctions.js
@@ -1,17 +1,6 @@
 const database = include('/databaseConnection');
 //Sohrab Stuff
-function getAllPosts(callback) {
-	let sqlQuery = "SELECT web_user_id, first_name, last_name, post_list, age FROM post_listing";
-	database.query(sqlQuery, (err, results, fields) => {
-		if (err) {
-			callback(err, null);
-		}
-		else {
-			console.log(results);
-			callback(null, results);
-		}		
-	});
-}
+
 
 
 
